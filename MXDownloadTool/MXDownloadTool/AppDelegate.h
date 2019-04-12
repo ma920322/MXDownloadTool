d32d29987309da3fc9fe77a45d47dd67af33af05
大好时光 <mcq@github.com>
2019-04-12 16:44:46 +0800

    初建项目

---
+++ MXDownloadTool/MXDownloadTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MXDownloadTool
+//
+//  Created by chengzi on 2019/4/12.
+//  Copyright © 2019 chengzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MXDownloadTool/MXDownloadTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MXDownloadTool
+//
+//  Created by chengzi on 2019/4/12.
+//  Copyright © 2019 chengzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
