22768df7939700d10992357808b972cb4a33ffb7
大好时光 <mcq@github.com>
2019-04-12 16:57:37 +0800

    修改问纯代码开发

--- MXDownloadTool/MXDownloadTool/MXDownloadViewController.h
+++ MXDownloadTool/MXDownloadTool/MXDownloadViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MXDownloadViewController.h
 //  MXDownloadTool
 //
 //  Created by chengzi on 2019/4/12.
@@ -8,8 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+NS_ASSUME_NONNULL_BEGIN
 
+@interface MXDownloadViewController : UIViewController
 
 @end
 
+NS_ASSUME_NONNULL_END
---
+++ MXDownloadTool/MXDownloadTool/MXDownloadViewController.m
@@ -0,0 +1,32 @@
+//
+//  MXDownloadViewController.m
+//  MXDownloadTool
+//
+//  Created by chengzi on 2019/4/12.
+//  Copyright © 2019 chengzi. All rights reserved.
+//
+
+#import "MXDownloadViewController.h"
+
+@interface MXDownloadViewController ()
+
+@end
+
+@implementation MXDownloadViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"下载";
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
